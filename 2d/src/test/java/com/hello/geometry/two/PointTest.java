c643adda5e30b14d12c6507458299419971ddfcc
scottmayberry <scott@canabrook.org>
2018-04-18 11:57:36 -0500

    Update test case

--- 2d/src/test/java/com/hello/geometry/two/PointTest.java
+++ 2d/src/test/java/com/hello/geometry/two/PointTest.java
@@ -10,5 +10,6 @@ public class PointTest {
     public void canConstruct() {
         Point p = new Point(5.0, 6.1);
         assertEquals(5.0, p.getX(), 0.0001);
+        assertEquals(6.1, p.getY(), 0.0001);
     }
 }
